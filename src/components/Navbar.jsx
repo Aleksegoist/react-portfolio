b50a749eac0e41c1581f744774949e6a2b9f5e8b
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-04 15:26:24 +0300

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => {
+    setNav(!nav);
+  };
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#13153a] text-gray-200'>
       <div>
-        <img src={Logo} alt='Logo' style={{ width: '130px' }} />
+        <img
+          src={Logo}
+          alt='Logo'
+          style={{ width: '130px', cursor: 'pointer' }}
+        />
       </div>
 
       {/* ----Menu----- */}
-      <div>
-        <ul className='flex '>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* ----Hamburger----- */}
-      <div className='hidden'>
-        <FaBars />
-      </div>
-
-      {/* ----Mobile Menu----- */}
-      <ul className='hidden'>
+      <ul className='hidden md:flex '>
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -34,8 +26,28 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
+      {/* ----Hamburger----- */}
+      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer '>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* ----Mobile Menu----- */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#13153a] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-3xl'>Home</li>
+        <li className='py-6 text-3xl'>About</li>
+        <li className='py-6 text-3xl'>Skills</li>
+        <li className='py-6 text-3xl'>Work</li>
+        <li className='py-6 text-3xl'>Contact</li>
+      </ul>
+
       {/* ----Social icons----- */}
-      <div></div>
+      <div className='hidden'> </div>
     </div>
   );
 };
