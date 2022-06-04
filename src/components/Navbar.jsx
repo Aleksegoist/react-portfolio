444695f4fe95ce86cfc695d4941ca2b36563a589
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-04 14:11:30 +0300

    add new files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#13153a] text-gray-200'>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
