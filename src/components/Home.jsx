90d8b5464af1dffeee6f987b15ef0ed314762e52
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-05 12:21:18 +0300

    add homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,27 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#13153a]'>
       {/* ----container--- */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p>Hi, my name is</p>
-        <h1>Alex Yehorov</h1>
-        <h2>I'm a Front-End developer.</h2>
+        <p className='text-[#FF9623]'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6] '>
+          Alex Yehorov
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          A Front-End Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I'm a front-end developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I'm focused on
+          building responsive web applications while learning back-end
+          technologies.
+        </p>
+        <div>
+          <button className='text-white group border-2 rounded-[8px] px-6 py-3 my-2 flex items-center hover:bg-[#FF9623] hover:border-[#FF9623]'>
+            View work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-2' />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
