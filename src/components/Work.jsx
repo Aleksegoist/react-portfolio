fd78a2adcd7734041b5f5f37395883af6dc5d69f
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-05 17:13:45 +0300

    add

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import webflix from '../assets/work_webflix.png';
+import github from '../assets/github.png';
 
 const Work = () => {
   return (
@@ -11,17 +12,53 @@ const Work = () => {
           </p>
           <p className='py-6'>// Check out some of my recent work</p>
         </div>
+
+        {/* ---Container--- */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          {/* ---Grid Item--- */}
+          <div
+            style={{ backgroundImage: `url(${webflix})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
             {/* ---Hover effect--- */}
-            <div>
-              <span></span>
-              <div>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React clone Netflix
+              </span>
+              <div className='pt-8 text-center'>
                 <a href='/'>
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
                 </a>
                 <a href='/'>
-                  <button></button>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${github})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* ---Hover effect--- */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React clone Netflix
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
